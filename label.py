ce8fc93e4f09a5b82b5d886c2ae8159ddd51ccef
kishorekumarvajravel <37907140+kishorekumarvajravel@users.noreply.github.com>
2019-12-14 14:29:14 +0530

    label creation (^:-+

---
+++ label.py
@@ -0,0 +1,9 @@
+from tkinter import *
+v=Tk()
+v.title("creating label")
+
+#creating label
+l=Label(v,text="Hi ,this is kishore !!! ",font=("times",20),bg='#1F3E34',fg='white')
+l.pack()
+
+mainloop()
